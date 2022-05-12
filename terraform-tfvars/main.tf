154b7ade9b5409bdee4ffe5d6da318c7afcdf3fe
kooskoos20 <omkarbirade20@gmail.com>
2022-05-12 23:17:39 +0530

    Initialise repo to spin an EC2 instance

---
+++ terraform-tfvars/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+//referencing ubuntu AMI
+data "aws_ami" "ubuntu" {
+  most_recent      = true
+  owners           = ["099720109477"] #Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+//spinning up billing server
+resource "aws_instance" "billing_server" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  tags = {
+    "service" = "billing"
+  }
+}
